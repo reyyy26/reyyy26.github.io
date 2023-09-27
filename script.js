03055eb6f423d9434f5e8eb3fe81021a9f2bbb46
reyyy26 <145451012+reyyy26@users.noreply.github.com>
2023-09-27 23:34:43 +0800

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-// toggle class active
-const navbarNav = document.querySelector('.navbar-nav')
-//ketika di klik
-document.querySelector('#hambruger-menu').onclick = () => {
-navbarNav.classList.toggle('active')
-};
