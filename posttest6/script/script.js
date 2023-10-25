7fe1bfe553daa51cd0a44cb2a256361484d5842a
reyyy26 <145451012+reyyy26@users.noreply.github.com>
2023-10-25 23:33:56 +0800

    Add files via upload

---
+++ posttest6/script/script.js
@@ -0,0 +1,6 @@
+// toggle class active
+const navbarNav = document.querySelector('.navbar-nav')
+//ketika di klik
+document.querySelector('#hambruger-menu').onclick = () => {
+navbarNav.classList.toggle('active')
+};
